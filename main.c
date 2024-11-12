bb1253496992f2005ae7d33d7b80e83e30742c93
ByNcosta <nonatocosta212@gmail.com>
2024-11-12 18:39:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    int n; 
+    int *vetor ; // Ponteiro para o vetor dinâmico
+    if (argc < 2) {
+        return 0; 
+    }
+    n = atoi(argv[1]);
+    
+    if (n == 0) {
+        return 0; 
+    }
+    vetor = (int *)malloc(n * sizeof(int));
+    if (vetor == NULL) {
+        return 1; 
+    }
+    for (int i = 0; i < n; i++) {
+        vetor[i] = atoi(argv[i + 2]); 
+    }
+    free(vetor);
     return 0;
 }
